ecfbb1961bc957b0cc4e7664d934b74c2f100e5a
Michal Dziedzic <michal.dziedzic@kuscheholding.com>
2023-12-09 18:37:18 +0100

    mobile layout general settings

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,11 @@
 <template>
-	<header>HEADERRRRRRR</header>
+  <div>
+    <v-img :src="pierogator_swiateczny"></v-img>
+  </div>
 </template>
+<script setup lang="ts">
+import pierogator_swiateczny from "@/assets/header/pierogator_swiateczny.svg";
+import pierogarnia from "@/assets/header/pierogarnia.svg";
+</script>
+
+<style lang="scss" scoped></style>
