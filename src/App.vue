b59a4f303f2a0bedde9e8d876b5eeb0c2f57a95f
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-09 18:14:17 +0100

    added fonts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <router-view />
+	<router-view />
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
+
+<style>
+@import url('./styles/typography.scss');
+
+#app {
+	font-family: 'Poppins';
+	font-weight: 400;
+}
+</style>
