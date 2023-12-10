c50badb802a664587343242730bc05c6cb8bb171
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-10 16:00:42 +0100

    dumplings store methods

--- src/store/dumplings.store.ts
+++ src/store/dumplings.store.ts
@@ -1,23 +1,44 @@
 import { defineStore } from 'pinia';
-
-export class Dumpling {
-	name = '';
-	imageUrl = '';
-}
-
+import { createRecipe } from '@/api/pierogator/createRecipe';
+import { deleteRecipe } from '@/api/pierogator/deleteRecipe';
+import { getAllRecipes } from '@/api/pierogator/getAllRecipes';
+import { getMyRecipes } from '@/api/pierogator/getMyRecipes';
+import { getRecipeById } from '@/api/pierogator/getRecipeById';
+import { Recipe } from '@/models/Recipe';
 interface DumplingsStore {
-	dumplings: Dumpling[];
+	allRecipes: Recipe[];
+	myRecipes: Recipe[];
+	currentRecipe?: Recipe;
 }
 
 const initState = (): DumplingsStore => ({
-	dumplings: [],
+	allRecipes: [],
+	myRecipes: [],
+	currentRecipe: undefined,
 });
 
 export const useDumplingsStore = defineStore('dumplingsStore', {
 	state: initState,
 	actions: {
-		addDumpling(dumpling: Dumpling): void {
-			this.dumplings.push(dumpling);
+		async getAllDumplings(): Promise<void> {
+			this.allRecipes = await getAllRecipes();
+		},
+		async getMyDumplings(): Promise<void> {
+			this.myRecipes = await getMyRecipes();
+		},
+		async getDumpling(recipeId: string): Promise<void> {
+			this.currentRecipe = await getRecipeById(recipeId);
+		},
+		async addDumpling(recipe: Recipe): Promise<void> {
+			await createRecipe(recipe);
+			this.fetchRecipes();
+		},
+		async removeDumpling(recipeId: string): Promise<void> {
+			await deleteRecipe(recipeId);
+			this.fetchRecipes();
+		},
+		async fetchRecipes(): Promise<void> {
+			await Promise.all([this.getAllDumplings(), this.getMyDumplings()]);
 		},
 	},
 });
