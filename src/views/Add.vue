74aec74b132eed8e87a00e8c50aae2fddec3fb4e
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-10 16:00:31 +0100

    minor template changes + create test recipe

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -9,19 +9,41 @@
 		:ingredients="['banany, czekolada']"
 		style="margin-bottom: 40px"
 	/>
-	<CTA :button-text="'Zapisz i przejdź do tworzenia przepisu'" />
+	<CTA
+		:button-text="'Zapisz i przejdź do tworzenia przepisu'"
+		@click="saveDumpling"
+	/>
 </template>
 
 <script setup lang="ts">
 import DumplingGenerator from '@/components/DumplingGenerator.vue';
 import CTA from '@/components/CTA.vue';
 import { ref } from 'vue';
-import { useDumplingsStore, Dumpling } from '../store/dumplings.store';
+import { useDumplingsStore } from '../store/dumplings.store';
+import { Recipe } from '@/models/Recipe';
+import { Ingredients } from '@/models/Ingredients';
 
 const dumplingStore = useDumplingsStore();
-const testDumpling = new Dumpling();
+const testRecipe = new Recipe();
+
+testRecipe.setName('test recipe...');
+testRecipe.setImageSrc(
+	'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5v3ZBADao2HdMWOdHdwDrMYoWFIApd_mXfQYZFCTaIw&s'
+);
+
+testRecipe.setIngredients({
+	dough: [{ name: 'kakao', quantity: '1 łyzka' }],
+	filling: [{ name: 'budyń', quantity: '15 opakowań' }],
+});
+
+testRecipe.setInstructions({
+	dough_preparation: ['doug preparation'],
+	filling_preparation: ['some filling prep #1', 'filling prep #2'],
+	forming_and_cooking_dumplings: ['forming _____ 123'],
+	serving: ['podawać z colą'],
+});
 
 function saveDumpling(): void {
-	dumplingStore.addDumpling(testDumpling);
+	dumplingStore.addDumpling(testRecipe);
 }
 </script>
