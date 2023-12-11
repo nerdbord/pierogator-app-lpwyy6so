15daec1a1d3cbe3eb6f83d10cbc3fc2f11fbf143
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-11 23:14:43 +0100

    ItemPage init, PreviewHeader finished

---
+++ src/components/PreviewHeader.vue
@@ -0,0 +1,52 @@
+<template>
+	<div>
+		<header class="d-flex justify-space-between align-center">
+			<div class="d-flex">
+				<slot></slot>
+			</div>
+			<h2 class="d-flex">
+				<v-img
+					height="30"
+					width="30"
+					:src="dumpling"
+					style="margin-right: 8px"
+				></v-img>
+				Pieróg
+			</h2>
+		</header>
+		<div class="d-flex flex-column">
+			<div class="preview__image--container">
+				<v-img :src="imgSrc" cover></v-img>
+			</div>
+			<div>
+				<Input :value="name" :input-id="name" :is-disabled="true" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import Loader from './Loader.vue';
+import Button from './Button.vue';
+import Input from './Input.vue';
+import dumpling from '../assets/dumpling.svg';
+
+const props = defineProps({
+	imgSrc: { type: String, required: true },
+	name: { type: String, required: true },
+});
+</script>
+
+<style scoped lang="scss">
+@import '../styles/variables.scss';
+.preview__image--container {
+	border-radius: $border-radius-base;
+	overflow: hidden;
+	width: 100%;
+	margin: 16px 0;
+	max-height: 280px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
--- src/views/ItemPage.vue
+++ src/views/ItemPage.vue
@@ -1,18 +1,44 @@
 <template>
-	<div>
-		<h1>item page / widok pieroga</h1>
-		<ul>
-			<li>- zdjęcie</li>
-			<li>
-				- przepis:
-				<ul>
-					asd
-					<li>uwagi do przepisu</li>
-					<li>akordeony (składniki ,przygotowanie, podawanie)</li>
-				</ul>
-			</li>
-
-			<li>- udostępnij button</li>
-		</ul>
+	<div class="d-flex flex-column" style="row-gap: 16px; column-gap: 20px">
+		<PreviewHeader
+			:img-src="dumplingsStore.currentRecipe?.imageSrc || ''"
+			:name="dumplingsStore.currentRecipe?.name || ''"
+		>
+			<Button :button-text="'Wróć'" @click="pushToPreviousPage" />
+		</PreviewHeader>
 	</div>
 </template>
+
+<script setup lang="ts">
+import PreviewHeader from '@/components/PreviewHeader.vue';
+import Button from '@/components/Button.vue';
+import router from '@/router';
+import { onBeforeMount, ref, watch } from 'vue';
+import { Recipe } from '@/models/Recipe';
+import { useDumplingsStore } from '@/store/dumplings.store';
+import { onUnmounted } from 'vue';
+
+const dumplingsStore = useDumplingsStore();
+const recipe = ref(new Recipe());
+
+onBeforeMount(() => {
+	dumplingsStore.getRecipeById(String(router.currentRoute.value.params.id));
+});
+
+onUnmounted(() => {
+	dumplingsStore.currentRecipe = undefined;
+});
+
+function pushToPreviousPage(): void {
+	router.go(-1);
+}
+
+watch(
+	() => dumplingsStore.currentRecipe?._id,
+	(id) => {
+		if (dumplingsStore.currentRecipe) {
+			recipe.value = dumplingsStore.currentRecipe;
+		} else recipe.value = new Recipe();
+	}
+);
+</script>
