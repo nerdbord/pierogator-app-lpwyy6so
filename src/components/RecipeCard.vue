eafd36340210aa608c9415113c09efa8763349cf
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-11 21:49:27 +0100

    MyRecipes section and RecipeCard

---
+++ src/components/RecipeCard.vue
@@ -0,0 +1,60 @@
+<template>
+	<div
+		class="d-flex flex-column"
+		style="width: calc(50% - 10px); margin-bottom: 20px; min-width: 170px"
+	>
+		<div class="card__image--container">
+			<v-img :src="imgSrc" cover></v-img>
+		</div>
+		<div>
+			<p class="card__name">{{ name }}</p>
+		</div>
+		<div v-if="isMyRecipe" class="d-flex" style="gap: 8px">
+			<Button :button-text="'Otwórz'" @click="pushToRecipeView" />
+			<Button :button-text="'Usuń'" @click="removeRecipe" />
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import router from '@/router';
+import Button from './Button.vue';
+import { RoutesNames } from '@/enums/RoutesNames.enum';
+import { useDumplingsStore } from '@/store/dumplings.store';
+
+const props = defineProps({
+	id: { type: String, required: true },
+	name: { type: String, required: true },
+	imgSrc: { type: String, required: true },
+	isMyRecipe: { type: Boolean, default: false },
+});
+
+const dumplingsStore = useDumplingsStore();
+
+function pushToRecipeView(): void {
+	router.push({ name: RoutesNames.ItemPage, params: { id: props.id } });
+}
+
+function removeRecipe(): void {
+	dumplingsStore.removeRecipe(props.id);
+}
+</script>
+
+<style scoped lang="scss">
+@import '../styles/variables.scss';
+
+.card__image--container {
+	border-radius: $border-radius-base;
+	overflow: hidden;
+	width: 100%;
+	height: 80%;
+	max-height: 140px;
+}
+
+.card__name {
+	font-size: $font-size-root;
+	font-weight: 400;
+	color: $col-dark-green;
+	margin: 8px 0;
+}
+</style>
