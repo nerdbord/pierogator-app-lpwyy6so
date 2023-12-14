07f383ccf27b0ac5697db0d66e96c6b0c2e5ffb0
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2023-12-14 15:26:57 +0100

    display generated resp in UI

--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -28,7 +28,11 @@
 				type="text"
 				placeholder="dodaj uwagi"
 			/>
-			<IngredientsAccordion />
+			<div v-if="isRecipeGenerated" class="d-flex flex-column" style="gap: 8px">
+				<IngredientsAccordion />
+				<PrepairAccordion />
+				<ServingAccordion />
+			</div>
 		</div>
 	</div>
 </template>
@@ -48,6 +52,9 @@ import {
 } from '@/api/openai/getRecipe';
 import { Recipe } from '@/models/Recipe';
 import { Ingredients } from '@/models/Ingredients';
+import { getPreparationSteps } from '@/api/openai/preparations';
+import ServingAccordion from '@/components/ServingAccordion.vue';
+import PrepairAccordion from '@/components/PrepairAccordion.vue';
 
 const dumplingsStore = useDumplingsStore();
 const { generatingRecipe, currentRecipeName, currentRecipeUrlImgPath } =
@@ -56,6 +63,7 @@ const { generatingRecipe, currentRecipeName, currentRecipeUrlImgPath } =
 dumplingsStore.currentRecipe = new Recipe();
 
 const notes = ref('');
+const isRecipeGenerated = ref(false);
 
 function pushToPreviousPage(): void {
 	router.push({ path: '/recipe/add' });
@@ -79,6 +87,8 @@ async function getDataForFirstAccordion(
 	});
 }
 
+// click -> generateRecipe() -> tworzy w sobie prompty -> getData... ktora przypisuje do store'a otrzymane value;
+
 async function generateRecipe() {
 	if (generatingRecipe?.value === undefined) {
 		return;
@@ -86,28 +96,32 @@ async function generateRecipe() {
 
 	const PrmptDough = `Przepis na ciasto na pierogi z składnikami: ${generatingRecipe.value.factors.factors.val}. Podaj tylko same proporcje składników i zawsze zwracaj w podanym formacie:"składnik:ilosć,składnik:ilosć,składnik:ilosć"`;
 	const PromptFill = `Przepis na Farsz na pierogi z 'malina,wisnia,jabłko'. Podaj tylko same proporcje składników i zawsze zwracaj w podanym formacie:"składnik:ilosć,składnik:ilosć,składnik:ilosć"`;
-	const dataForFirstAccordion = await getDataForFirstAccordion(
-		PrmptDough,
-		PromptFill
-	);
+	await getDataForFirstAccordion(PrmptDough, PromptFill);
 
-	// getDataForSecondAccordion; // 2nd accordion includes: getDoughPreparationDetails & getFillPreparationDetails &
-	// this.dough_preparation = instructions.dough_preparation;
-	// 	this.filling_preparation = instructions.filling_preparation;
-	// 	this.forming_and_cooking_dumplings =
-	// 		instructions.forming_and_cooking_dumplings;
+	const doughPreparationPrompt = `Wygeneruj instrukcje  krok po kroku potrzebna do stworzenia ciasta na pierogi z podanych składników: §${generatingRecipe.value.factors.cake.val}. Zawsze zwracaj jako  ciąg znaków. Każdy krok jest od siebie oddzielony znakiem przecinka i oznacza dana czynnosc do wykonania. Przykładowy format: 'step','next step'. Pomiń numerowanie oraz przejscie do następnej lini`;
+	const fillingPreparationPrompt = `Wygeneruj instrukcje  krok po kroku potrzebna do stworzenia nadzienia/farszu do pierogów z podanych składników: ${generatingRecipe.value.factors.feelings.val}. Zawsze zwracaj jako  ciąg znaków. Każdy krok jest od siebie oddzielony znakiem przecinka. Przykładowy format: 'step','next step'. Pomiń numerowanie oraz przejscie do następnej linii.`;
+	const formingAndCookingPreparationPrompt = `Wygeneruj instrukcje formowania i gotowania pierogów zakładając, ze mam juz przygotowane ciasto i farsz. Zawsze zwracaj jako  ciąg znaków. Każdy krok jest od siebie oddzielony znakiem przecinka. Przykładowy format: 'step','next step'. Pomiń numerowanie oraz przejscie do następnej linii. Pomiń instrukcje podawania/serwowania tych pierogów. Uwzględniając podane uwagi: ${notes.value}`;
+	const servingPreparationPrompt = `Wygeneruj instrukcje serwowania/podawania pierogów po ugotowaniu. Maxymalnie 200 znaków. Uwzględnij podane uwagi: ${notes.value}.`;
 
-	// getDataForThirdAccordion // 3rd includes only Serving instructions:
-	// this.serving = api res;
+	async function getInstructions() {
+		const [dough_preparation, filling_preparation] = await Promise.all([
+			getPreparationSteps(doughPreparationPrompt),
+			getPreparationSteps(fillingPreparationPrompt),
+		]);
 
-	// console.log(data);
-
-	// if (data) {
-	// 	const testPropmpt1: Ingredient[] = parseIngredients(data);
-
-	// 	// console.log(testPropmpt1);
-	// 	//dumplingsStore.currentRecipe?.setIngredients(testPropmpt1);
-	// }
+		const [forming_and_cooking_dumplings, serving] = await Promise.all([
+			getPreparationSteps(formingAndCookingPreparationPrompt),
+			getPreparationSteps(servingPreparationPrompt),
+		]);
+		dumplingsStore.currentRecipe?.setInstructions({
+			dough_preparation,
+			filling_preparation,
+			forming_and_cooking_dumplings,
+			serving,
+		});
+	}
+	getInstructions();
+	isRecipeGenerated.value = true;
 }
 </script>
 
