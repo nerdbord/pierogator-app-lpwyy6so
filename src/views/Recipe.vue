2b4ef5bf7e7c1d336ea884a937029275bc75e7f4
Michal Dziedzic <michal.dziedzic@kuscheholding.com>
2023-12-14 20:03:34 +0100

    Co-authored-by: lukaasz555 <lukaasz555@users.noreply.github.com>

--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -1,153 +1,153 @@
 <template>
-	<div>
-		<SectionHeader :header-text="'Pieróg'">
-			<Button :button-text="'Zmień'" @click="pushToPreviousPage" />
-		</SectionHeader>
-		<div>
-			<input
-				v-model="currentRecipeName"
-				style="width: 100%; outline: none"
-				type="text"
-				placeholder="Enter a Tag"
-				disabled="true"
-			/>
-			<main v-if="currentRecipeUrlImgPath" class="picture--container">
-				<v-img
-					:width="'100%'"
-					:max-height="300"
-					:src="currentRecipeUrlImgPath"
-					cover
-				></v-img>
-			</main>
-			<SectionHeader :header-text="'Przepis'">
-				<Button :button-text="'Generuj'" @click="generateRecipe()" />
-			</SectionHeader>
-			<input
-				:v-model="notes"
-				style="width: 100%; outline: none"
-				type="text"
-				placeholder="dodaj uwagi"
-			/>
-			<div v-if="isRecipeGenerated" class="d-flex flex-column" style="gap: 8px">
-				<IngredientsAccordion />
-				<PrepairAccordion />
-				<ServingAccordion />
-				<CTA :button-text="'Udostępnij pieroga'" @click="createDumpling" />
-			</div>
-		</div>
-	</div>
+  <div>
+    <SectionHeader :header-text="'Pieróg'">
+      <Button :button-text="'Zmień'" @click="pushToPreviousPage" />
+    </SectionHeader>
+    <div>
+      <input
+        v-model="currentRecipeName"
+        style="width: 100%; outline: none"
+        type="text"
+        placeholder="Enter a Tag"
+        disabled="true"
+      />
+      <main v-if="currentRecipeUrlImgPath" class="picture--container">
+        <v-img
+          :width="'100%'"
+          :max-height="300"
+          :src="currentRecipeUrlImgPath"
+          cover
+        ></v-img>
+      </main>
+      <SectionHeader :header-text="'Przepis'">
+        <Button :button-text="'Generuj'" @click="generateRecipe()" />
+      </SectionHeader>
+      <input
+        :v-model="notes"
+        style="width: 100%; outline: none"
+        type="text"
+        placeholder="dodaj uwagi"
+      />
+      <div v-if="isRecipeGenerated" class="d-flex flex-column" style="gap: 8px">
+        <IngredientsAccordion />
+        <PrepairAccordion />
+        <ServingAccordion />
+        <CTA :button-text="'Udostępnij pieroga'" @click="createDumpling" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import router from '@/router';
-import { ref } from 'vue';
-import { storeToRefs } from 'pinia';
-import { useDumplingsStore } from '@/store/dumplings.store';
-import IngredientsAccordion from '@/components/IngredientsAccordion.vue';
-import SectionHeader from '@/components/SectionHeader.vue';
-import Button from '@/components/Button.vue';
-import InputPromp from '@/components/InputPromp.vue';
+import router from "@/router";
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useDumplingsStore } from "@/store/dumplings.store";
+import IngredientsAccordion from "@/components/IngredientsAccordion.vue";
+import SectionHeader from "@/components/SectionHeader.vue";
+import Button from "@/components/Button.vue";
+import InputPromp from "@/components/InputPromp.vue";
 import {
-	getDoughIngredientsDetails,
-	getFillIngredientsDetails,
-} from '@/api/openai/getRecipe';
-import { Recipe } from '@/models/Recipe';
-import { Ingredients } from '@/models/Ingredients';
-import { getPreparationSteps } from '@/api/openai/preparations';
-import ServingAccordion from '@/components/ServingAccordion.vue';
-import PrepairAccordion from '@/components/PrepairAccordion.vue';
-import CTA from '@/components/CTA.vue';
-import { onUnmounted } from 'vue';
-import { ICreateRecipe } from '@/api/pierogator/createRecipe';
-import { RoutesNames } from '@/enums/RoutesNames.enum';
-import { useGlobalStore } from '@/store/app';
+  getDoughIngredientsDetails,
+  getFillIngredientsDetails,
+} from "@/api/openai/getRecipe";
+import { Recipe } from "@/models/Recipe";
+import { Ingredients } from "@/models/Ingredients";
+import { getPreparationSteps } from "@/api/openai/preparations";
+import ServingAccordion from "@/components/ServingAccordion.vue";
+import PrepairAccordion from "@/components/PrepairAccordion.vue";
+import CTA from "@/components/CTA.vue";
+import { onUnmounted } from "vue";
+import { ICreateRecipe } from "@/api/pierogator/createRecipe";
+import { RoutesNames } from "@/enums/RoutesNames.enum";
+import { useGlobalStore } from "@/store/app";
 
 const dumplingsStore = useDumplingsStore();
 const { generatingRecipe, currentRecipeName, currentRecipeUrlImgPath } =
-	storeToRefs(dumplingsStore);
+  storeToRefs(dumplingsStore);
 
-const notes = ref('');
+const notes = ref("");
 const isRecipeGenerated = ref(false);
 
 function pushToPreviousPage(): void {
-	router.push({ path: '/recipe/add' });
+  router.push({ path: "/recipe/add" });
 }
 
 interface Ingredient {
-	name: string;
-	quantity: string;
+  name: string;
+  quantity: string;
 }
 
 async function getDataForFirstAccordion(
-	doughPrompt: string,
-	fillPrompt: string
+  doughPrompt: string,
+  fillPrompt: string
 ) {
-	const dough = await getDoughIngredientsDetails(doughPrompt);
-	const filling = await getFillIngredientsDetails(fillPrompt);
+  const dough = await getDoughIngredientsDetails(doughPrompt);
+  const filling = await getFillIngredientsDetails(fillPrompt);
 
-	if (dumplingsStore.currentRecipe) {
-		dumplingsStore.currentRecipe?.setIngredients({
-			dough,
-			filling,
-		});
-	} else {
-		throw Error('There is no recipe in store');
-	}
+  if (dumplingsStore.currentRecipe) {
+    dumplingsStore.currentRecipe?.setIngredients({
+      dough,
+      filling,
+    });
+  } else {
+    throw Error("There is no recipe in store");
+  }
 }
 
 async function generateRecipe() {
-	if (generatingRecipe?.value === undefined) {
-		return;
-	}
+  if (generatingRecipe?.value === undefined) {
+    return;
+  }
 
-	const PrmptDough = `Przepis na ciasto na pierogi z składnikami: ${generatingRecipe.value.factors.factors.val}. Podaj tylko same proporcje składników i zawsze zwracaj w podanym formacie:"składnik:ilosć,składnik:ilosć,składnik:ilosć"`;
-	const PromptFill = `Przepis na Farsz na pierogi z 'malina,wisnia,jabłko'. Podaj tylko same proporcje składników i zawsze zwracaj w podanym formacie:"składnik:ilosć,składnik:ilosć,składnik:ilosć"`;
-	await getDataForFirstAccordion(PrmptDough, PromptFill);
+  const PrmptDough = `Przepis na ciasto na pierogi z składnikami: ${generatingRecipe.value.factors.factors.val}. Podaj tylko same proporcje składników i zawsze zwracaj w podanym formacie:"składnik:ilosć,składnik:ilosć,składnik:ilosć"`;
+  const PromptFill = `Przepis na Farsz na pierogi z 'malina,wisnia,jabłko'. Podaj tylko same proporcje składników i zawsze zwracaj w podanym formacie:"składnik:ilosć,składnik:ilosć,składnik:ilosć"`;
+  await getDataForFirstAccordion(PrmptDough, PromptFill);
 
-	const doughPreparationPrompt = `Wygeneruj instrukcje  krok po kroku potrzebna do stworzenia ciasta na pierogi z podanych składników: §${generatingRecipe.value.factors.cake.val}. Zawsze zwracaj jako  ciąg znaków. Każdy krok jest od siebie oddzielony znakiem przecinka i oznacza dana czynnosc do wykonania. Przykładowy format: 'step','next step'. Pomiń numerowanie oraz przejscie do następnej lini`;
-	const fillingPreparationPrompt = `Wygeneruj instrukcje  krok po kroku potrzebna do stworzenia nadzienia/farszu do pierogów z podanych składników: ${generatingRecipe.value.factors.feelings.val}. Zawsze zwracaj jako  ciąg znaków. Każdy krok jest od siebie oddzielony znakiem przecinka. Przykładowy format: 'step','next step'. Pomiń numerowanie oraz przejscie do następnej linii.`;
-	const formingAndCookingPreparationPrompt = `Wygeneruj instrukcje formowania i gotowania pierogów zakładając, ze mam juz przygotowane ciasto i farsz. Zawsze zwracaj jako  ciąg znaków. Każdy krok jest od siebie oddzielony znakiem przecinka. Przykładowy format: 'step','next step'. Pomiń numerowanie oraz przejscie do następnej linii. Pomiń instrukcje podawania/serwowania tych pierogów. Uwzględniając podane uwagi: ${notes.value}`;
-	const servingPreparationPrompt = `Wygeneruj instrukcje serwowania/podawania pierogów po ugotowaniu. Maxymalnie 200 znaków. Uwzględnij podane uwagi: ${notes.value}.`;
+  const doughPreparationPrompt = `Wygeneruj instrukcje  krok po kroku potrzebna do stworzenia ciasta na pierogi z podanych składników: §${generatingRecipe.value.factors.cake.val}. Zawsze zwracaj jako  ciąg znaków. Każdy krok jest od siebie oddzielony znakiem przecinka i oznacza dana czynnosc do wykonania. Przykładowy format: 'step','next step'. Pomiń numerowanie oraz przejscie do następnej lini`;
+  const fillingPreparationPrompt = `Wygeneruj instrukcje  krok po kroku potrzebna do stworzenia nadzienia/farszu do pierogów z podanych składników: ${generatingRecipe.value.factors.feelings.val}. Zawsze zwracaj jako  ciąg znaków. Każdy krok jest od siebie oddzielony znakiem przecinka. Przykładowy format: 'step','next step'. Pomiń numerowanie oraz przejscie do następnej linii.`;
+  const formingAndCookingPreparationPrompt = `Wygeneruj instrukcje formowania i gotowania pierogów zakładając, ze mam juz przygotowane ciasto i farsz. Zawsze zwracaj jako  ciąg znaków. Każdy krok jest od siebie oddzielony znakiem przecinka. Przykładowy format: 'step','next step'. Pomiń numerowanie oraz przejscie do następnej linii. Pomiń instrukcje podawania/serwowania tych pierogów. Uwzględniając podane uwagi: ${notes.value}`;
+  const servingPreparationPrompt = `Wygeneruj instrukcje serwowania/podawania pierogów po ugotowaniu. Maxymalnie 200 znaków. Uwzględnij podane uwagi: ${notes.value}.`;
 
-	async function getInstructions() {
-		const [dough_preparation, filling_preparation] = await Promise.all([
-			getPreparationSteps(doughPreparationPrompt),
-			getPreparationSteps(fillingPreparationPrompt),
-		]);
+  async function getInstructions() {
+    const [dough_preparation, filling_preparation] = await Promise.all([
+      getPreparationSteps(doughPreparationPrompt),
+      getPreparationSteps(fillingPreparationPrompt),
+    ]);
 
-		const [forming_and_cooking_dumplings, serving] = await Promise.all([
-			getPreparationSteps(formingAndCookingPreparationPrompt),
-			getPreparationSteps(servingPreparationPrompt),
-		]);
-		dumplingsStore.currentRecipe?.setInstructions({
-			dough_preparation,
-			filling_preparation,
-			forming_and_cooking_dumplings,
-			serving,
-		});
-	}
-	getInstructions();
-	isRecipeGenerated.value = true;
+    const [forming_and_cooking_dumplings, serving] = await Promise.all([
+      getPreparationSteps(formingAndCookingPreparationPrompt),
+      getPreparationSteps(servingPreparationPrompt),
+    ]);
+    dumplingsStore.currentRecipe?.setInstructions({
+      dough_preparation,
+      filling_preparation,
+      forming_and_cooking_dumplings,
+      serving,
+    });
+  }
+  getInstructions();
+  isRecipeGenerated.value = true;
 }
 
 async function createDumpling(): Promise<void> {
-	if (dumplingsStore.currentRecipe) {
-		const payload: ICreateRecipe =
-			dumplingsStore.currentRecipe.getPostPayload();
-		dumplingsStore.addRecipe(payload).then((res) => {
-			const globalStore = useGlobalStore();
-			globalStore.addErrorMessage(`${res.name} został dodany!`);
-			router.push({ name: RoutesNames.Main });
-			// dumplingsStore.fetchRecipes();
-		});
-	} else {
-		throw Error('There is no recipe in store');
-	}
+  if (dumplingsStore.currentRecipe) {
+    const payload: ICreateRecipe =
+      dumplingsStore.currentRecipe.getPostPayload();
+    dumplingsStore.addRecipe(payload).then((res) => {
+      const globalStore = useGlobalStore();
+      globalStore.addErrorMessage(`${res.name} został dodany!`);
+      router.push({ name: RoutesNames.Main });
+      // dumplingsStore.fetchRecipes();
+    });
+  } else {
+    throw Error("There is no recipe in store");
+  }
 }
 
-onUnmounted(() => {
-	dumplingsStore.currentRecipe = undefined;
-});
+// onUnmounted(() => {
+// 	dumplingsStore.currentRecipe = undefined;
+// });
 </script>
 
 <style type="scss" scoped></style>
